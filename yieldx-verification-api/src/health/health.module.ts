012ce98cf45e215fd76835b47e3b92e7c0263320
Osiyomeoh <samuelaleonomoh5@gmail.com>
2025-06-29 16:36:47 +0100

    YieldX Protocol with contracts, frontend, and backend

--- yieldx-verification-api/src/health/health.module.ts
+++ yieldx-verification-api/src/health/health.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { HealthService } from './health.service';
 import { HealthController } from './health.controller';
 import { PingController } from './ping.controller';
+import { PingService } from './ping.service';
+import { PingCron } from './ping.cron';
 
 @Module({
-  providers: [HealthService],
+  imports: [ScheduleModule.forRoot()],
+  providers: [HealthService, PingService, PingCron],
   controllers: [HealthController, PingController]
 })
 export class HealthModule {}
